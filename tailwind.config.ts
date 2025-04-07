d33698647861909cfce4022a4f58793e3ba84fec
Kirill Batomunkuev <i@batomunkuevv.ru>
2025-04-07 18:48:52 +0900

    Add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,38 @@
 import type { Config } from "tailwindcss";
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+const config: Config = {
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        screens: {
+            lg: { max: "1885px" },
+            md: { max: "1419px" },
+            tb: { max: "992px" },
+            sm: { max: "760px" },
+            s: { max: "576px" },
+            xs: { max: "359px" },
+        },
+        extend: {
+            colors: {
+                t: {
+                    white: "var(--text-white)",
+                    black: "var(--text-black)",
+                },
+                b: {
+                    primary: "var(--border-primary)",
+                },
+                g: {
+                    black: "var(--general-black)",
+                    gradient: {
+                        red: "var(--general-red-gradient)",
+                    },
+                },
+            },
+            fontFamily: {
+                helios: "var(--font-helios)",
+                benzin: "var(--font-benzin)",
+            },
+        },
     },
-  },
-  plugins: [],
-} satisfies Config;
+    plugins: [],
+};
+export default config;
